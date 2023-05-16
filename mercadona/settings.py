108c426451701c5610cbe7f9c58c7ae8a86df129
Filipouzo <philiparnal@gmail.com>
2023-05-16 13:14:29 +0200

    add AWS S3 configuration

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -162,7 +162,7 @@ if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_S3_REGION_NAME = 'eu-west-3'  # e.g. us-east-1
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KE
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
     # Tell django-storages the domain to use to refer to static files.
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
