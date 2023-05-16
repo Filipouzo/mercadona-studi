e2f40234edc5989155965b06e5bbf415ca8d6a9e
Filipouzo <philiparnal@gmail.com>
2023-05-16 15:29:44 +0200

    do not need product form anymore

--- offers/urls.py
+++ offers/urls.py
@@ -7,7 +7,6 @@ from .views import offers_list, product_create, page_admin
 urlpatterns = [
 
     path('', offers_list, name='offers_list'),
-    path('form/', product_create, name='product_form'),
     path('admin/', page_admin, name='page_admin'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
